977bf4a81b43b4367d877a8db8f19725ab7261af
M. Khairul Tegar <167969654+mkhairultegar@users.noreply.github.com>
2026-05-05 18:59:05 +0700

    Update project-detail.js

--- project-detail.js
+++ project-detail.js
@@ -264,4 +264,14 @@ const projectDetail = {
       .replace(/"/g, '&quot;')
       .replace(/\n/g, '<br>');
   }
+  // tandai notif project ini sebagai sudah dibaca
+  const snap = await db.collection('notifications')
+    .where('target_uid', '==', dashboard.currentUser.uid)
+    .where('project_id', '==', projectId)
+    .where('is_read', '==', false)
+    .get();
+  
+  const batch = db.batch();
+  snap.docs.forEach(d => batch.update(d.ref, { is_read: true }));
+  await batch.commit();
 };
